5b00af9842013bc7d6bd0cf13e8b92987c266133
ujjwalEminds <ujjwal.tiwari@eminds.ai>
2023-11-27 11:44:11 +0530

    feat: api integration of init and confirm

--- apps/odr/pages/orderConfirmation.tsx
+++ apps/odr/pages/orderConfirmation.tsx
@@ -24,99 +24,19 @@ const OrderConfirmation = () => {
 
   useEffect(() => {
     if (initResponse) {
-      const initMetaDataPerBpp = getInitMetaDataPerBpp(initResponse)
-
-      const payLoadForConfirmRequest = getPayloadForConfirmRequest(
-        initMetaDataPerBpp,
-        transactionId,
-        localStorage.getItem('userPhone') as string
-      )
-      confirmRequest.fetchData(`${apiUrl}/client/v2/confirm`, 'POST', payLoadForConfirmRequest)
+      const payLoadForConfirmRequest = getPayloadForConfirmRequest(initResponse, transactionId)
+      confirmRequest.fetchData(`${apiUrl}/confirm`, 'POST', payLoadForConfirmRequest)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   useEffect(() => {
     if (!initResponse && localStorage && localStorage.getItem('initResult')) {
-      const parsedInitResult = JSON.parse(localStorage.getItem('initResult') as string)
-      const initMetaDataPerBpp = getInitMetaDataPerBpp(parsedInitResult)
-
-      const payLoadForConfirmRequest = getPayloadForConfirmRequest(
-        initMetaDataPerBpp,
-        transactionId,
-        localStorage.getItem('userPhone') as string
-      )
-      confirmRequest.fetchData(`${apiUrl}/client/v2/confirm`, 'POST', payLoadForConfirmRequest)
+      const payLoadForConfirmRequest = getPayloadForConfirmRequest(initResponse, transactionId)
+      confirmRequest.fetchData(`${apiUrl}/confirm`, 'POST', payLoadForConfirmRequest)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-
-  // useEffect(() => {
-  //   if (confirmRequest.data) {
-  //     const bearerToken = Cookies.get('authToken')
-  //     const context = confirmRequest.data[0].message.responses[0].context
-  //     const order = confirmRequest.data[0].message.responses[0].message.order
-
-  //     const orderPayLoad = {
-  //       context: context,
-  //       message: {
-  //         order: {
-  //           id: order.id,
-  //           provider: {
-  //             id: order.provider.id,
-  //             descriptor: order.provider.descriptor
-  //           },
-  //           items: [
-  //             {
-  //               id: order.items[0].id,
-  //               descriptor: order.items[0].descriptor,
-  //               price: {
-  //                 value: order.items[0].price.value,
-  //                 currency: order.items[0].price.currency
-  //               },
-  //               tags: order.items[0].tags
-  //             }
-  //           ],
-  //           fulfillments: [order.fulfillment],
-  //           billing: {
-  //             name: order.billing.name,
-  //             address: order.billing.address.door,
-  //             email: order.billing.email,
-  //             phone: order.billing.phone
-  //           },
-  //           quote: {
-  //             price: order.quote.price,
-  //             breakup: order.quote.breakup
-  //           },
-  //           payments: [
-  //             {
-  //               type: order.payment.type,
-  //               status: order.payment.status,
-  //               params: {
-  //                 amount: order.payment.params.amount,
-  //                 currency: order.payment.params.currency
-  //               }
-  //             }
-  //           ]
-  //         }
-  //       },
-  //       category: {
-  //         set: [1]
-  //       }
-  //     }
-
-  //     const axiosConfig = {
-  //       headers: {
-  //         Authorization: `Bearer ${bearerToken}`,
-  //         'Content-Type': 'application/json' // You can set the content type as needed
-  //       }
-  //     }
-  //     axios
-  //       .post(`${strapiUrl}/orders`, orderPayLoad, axiosConfig)
-  //       .then(res => {})
-  //       .catch(err => console.error(err))
-  //   }
-  // }, [confirmRequest.data])
 
   if (confirmRequest.loading) {
     return (
@@ -126,16 +46,16 @@ const OrderConfirmation = () => {
       />
     )
   }
-  const handleViewCource = () => {
+  const handleViewOrder = () => {
     if (confirmRequest.data) {
       localStorage.setItem('confirmData', JSON.stringify(confirmRequest.data))
       router.push('/orderDetails')
     }
   }
 
-  const learningHistoryHandler = (): void => {
-    router.push('/myLearningOrderHistory')
-  }
+  const agentName = confirmRequest.data?.scholarshipProvider?.scholarships[0]?.scholarshipDetails?.agentDetails?.name
+  const agentNumber =
+    confirmRequest.data?.scholarshipProvider?.scholarships[0]?.scholarshipDetails?.agentDetails?.contactDetails?.phone
 
   return (
     <Box>
@@ -157,14 +77,14 @@ const OrderConfirmation = () => {
                   src={nameIcon}
                   pr={'12px'}
                 />
-                <Text fontSize={'15px'}>Adv. Vishal Singh</Text>
+                <Text fontSize={'15px'}>{agentName}</Text>
               </Flex>
               <Flex alignItems={'center'}>
                 <Image
                   src={CallphoneIcon}
                   pr={'12px'}
                 />
-                <Text fontSize={'15px'}>+91 9832445890</Text>
+                <Text fontSize={'15px'}>{agentNumber}</Text>
               </Flex>
             </Stack>
             <Stack>
@@ -195,7 +115,7 @@ const OrderConfirmation = () => {
           background={'rgba(var(--color-primary))'}
           color={'rgba(var(--text-color))'}
           isDisabled={false}
-          handleOnClick={handleViewCource}
+          handleOnClick={handleViewOrder}
         />
         <Button
           buttonText={t.backToHome}
