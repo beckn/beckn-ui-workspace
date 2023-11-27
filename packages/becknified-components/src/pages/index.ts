61b8f757b1eb52822669dd3883b4fe172621e283
Ujjwal Tiwari <ujjwaltiwari@Ujjwals-MacBook-Pro.local>
2023-11-27 10:40:59 +0530

    feat: adding product description page component

--- packages/becknified-components/src/pages/index.ts
+++ packages/becknified-components/src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as BecknAuth } from './auth'
 export { default as SearchResults } from './search-results'
+export { default as ProductDetailPage } from './product-detail'
---
+++ packages/becknified-components/src/pages/product-detail/product-detail.tsx
@@ -0,0 +1,39 @@
+import { Button } from '@beckn-ui/molecules'
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import { ProductDescription, ProductSummary } from '../../components'
+
+{
+  /* <WIP></WIP> */
+}
+
+const ProductDetailPage = () => {
+  return (
+    <>
+      <ProductSummary
+        name="hajsdajgd ahgdha ahsgdahsgdhagsd hags"
+        imageSrc="https://tourism-bpp-infra2.becknprotocol.io/attachments/view/292.jpg?imwidth=750"
+      />
+      <ProductDescription
+        description="<div>
+            <p><b>About the course:</b> This course will help you set up WordPress on AWS EC2, link to the RDS database, build a reliable NodeJS backend using Elastic Beanstalk, manage files via AWS S3, send emails with SES, and set CloudWatch alarms.</p>
+            <p><b>Course details:</b></p>
+            <p>Course Provider: ShopNotch</p>
+            <p>Course Instructor: BackSpace Academy</p>
+            <p>Course Length: 3 hours</p>
+            <p>Modules: 14</p>
+            <p>Skills taught: AWS, NodeJS</p>
+            <p>Level: Beginner</p>
+            <p>Schedule: Flexible</p>
+            <p>Course delivery: Online</p>
+        </div>"
+        // description="taushd asjhd ajshdjahsd ajhsdjahsd jh jahsjd ajshdjahsd jahs djhas jhdwjd  jhasjhajshdjahsd jhs djahs dajhds "
+      />
+      <Box mt={'20px'}>
+        <Button text="Proceed" />
+      </Box>
+    </>
+  )
+}
+
+export default ProductDetailPage
