b5329cb0b2556e6946fd785fba33e9b7002751bb
Ujjwal Tiwari <ujjwaltiwari@Ujjwals-MacBook-Pro.local>
2023-12-08 14:46:08 +0530

    feat: pushing accordion component

--- packages/molecules/src/components/accordion/Accordion.tsx
+++ packages/molecules/src/components/accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React from 'react'
 import {
   Accordion as AccordionComp,
   AccordionButton,
@@ -7,52 +7,52 @@ import {
   AccordionPanel,
   Box,
   Card,
-  CardBody,
-  Text
+  CardBody
 } from '@chakra-ui/react'
+import { AccordionProps } from './accordion.types'
 
-interface AccordionPropsModel {
-  children?: React.ReactNode
-  accordionHeader?: string | ReactElement
-}
-
-const Accordion: React.FC<AccordionPropsModel> = props => {
+const Accordion: React.FC<AccordionProps> = props => {
+  const { accordionHeader, children, onToggle, className = '' } = props
   return (
-    <>
-      <Card
-        marginBottom={'20px'}
-        className="border_radius_all"
+    <Card>
+      <AccordionComp
+        onChange={onToggle}
+        allowMultiple
+        className={`${className}-accordion-container`}
       >
-        <AccordionComp allowMultiple>
-          <AccordionItem
-            background={'unset'}
-            border={'unset'}
-            boxShadow={'0px 8px 10px -6px rgb(0 0 0 / 10%), 0px 20px 25px -5px rgb(0 0 0 / 10%)'}
+        <AccordionItem
+          background={'unset'}
+          border={'unset'}
+          boxShadow={'0px 8px 10px -6px rgb(0 0 0 / 10%), 0px 20px 25px -5px rgb(0 0 0 / 10%)'}
+        >
+          <CardBody padding={'15px 20px'}>
+            <AccordionButton
+              padding={'unset'}
+              background={'unset !important'}
+            >
+              <Box
+                as="span"
+                flex="1"
+                textAlign="left"
+                fontSize={'17px'}
+                fontWeight={'600'}
+                className={`${className}-accordion-header-text`}
+              >
+                {accordionHeader}
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </CardBody>
+          <AccordionPanel
+            className={`${className}-accordion-content`}
+            padding={'unset'}
           >
-            <CardBody padding={'15px 20px'}>
-              <h2>
-                <AccordionButton
-                  padding={'unset'}
-                  background={'unset !important'}
-                >
-                  <Box
-                    as="span"
-                    flex="1"
-                    textAlign="left"
-                    fontSize={'17px'}
-                    fontWeight={'600'}
-                  >
-                    {props.accordionHeader}
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-            </CardBody>
-            <AccordionPanel padding={'unset'}>{props.children}</AccordionPanel>
-          </AccordionItem>
-        </AccordionComp>
-      </Card>
-    </>
+            {children}
+          </AccordionPanel>
+        </AccordionItem>
+      </AccordionComp>
+    </Card>
   )
 }
+
 export default Accordion
