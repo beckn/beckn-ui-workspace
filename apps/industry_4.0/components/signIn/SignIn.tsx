f7552e0565e0945a176ffd7f3f3637553bbe7517
kundan singh <kundansingh@kundans-MacBook-Pro.local>
2023-12-15 11:08:10 +0530

    feat: initial setup for industry 4.0

---
+++ apps/industry_4.0/components/signIn/SignIn.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react'
+import Suppliflow_logo from '../../public/images/Suppliflow_logo.svg'
+import { useLanguage } from '@hooks/useLanguage'
+import { SignInPropsModel } from './SignIn.types'
+import { FormErrors, signInValidateForm } from '@utils/detailsForm-utils'
+import { BecknAuth } from '@beckn-ui/becknified-components'
+import Router from 'next/router'
+
+const SignIn = () => {
+  const { t } = useLanguage()
+
+  const [formData, setFormData] = useState<SignInPropsModel>({ email: '', password: '' })
+  const [formErrors, setFormErrors] = useState<FormErrors>({ email: '', password: '' })
+  const [isFormFilled, setIsFormFilled] = useState(true)
+  const baseUrl = process.env.NEXT_PUBLIC_STRAPI_URL
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+
+    setFormData((prevFormData: SignInPropsModel) => ({
+      ...prevFormData,
+      [name]: value
+    }))
+
+    const updatedFormData = {
+      ...formData,
+      [name]: value
+    }
+
+    // const errors = signInValidateForm(updatedFormData) as any
+    // setFormErrors(prevErrors => ({
+    //   ...prevErrors,
+    //   [name]: errors[name] || ''
+    // }))
+    setIsFormFilled(updatedFormData.email.trim() !== '' && updatedFormData.password.trim() !== '')
+  }
+
+  const handleSignIn = async () => {
+    const signInData = {
+      identifier: formData.email,
+      password: formData.password
+    }
+
+    try {
+      const response = await fetch(`${baseUrl}/auth/local`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(signInData)
+      })
+
+      if (response.ok) {
+        const data = await response.json()
+        const token = data.jwt
+
+        localStorage.setItem('token', token)
+        Router.push('/homePage')
+      } else {
+        console.error('Sign In failed')
+      }
+    } catch (error) {
+      console.error('An error occurred:', error)
+    }
+  }
+
+  return (
+    <BecknAuth
+      schema={{
+        logo: {
+          src: Suppliflow_logo,
+          alt: 'Suppliflow logo'
+        },
+        buttons: [
+          {
+            text: 'SignIn',
+            handleClick: handleSignIn,
+            disabled: !isFormFilled,
+            variant: 'solid',
+            colorScheme: 'primary'
+          },
+          {
+            text: 'SignUp',
+            handleClick: () => {
+              Router.push('/signUp')
+            },
+            disabled: false,
+            variant: 'outline',
+            colorScheme: 'primary'
+          }
+        ],
+        inputs: [
+          {
+            type: 'text',
+            name: 'email',
+            value: formData.email,
+            handleChange: handleInputChange,
+            label: 'Email'
+          },
+          {
+            type: 'password',
+            name: 'password',
+            value: formData.password,
+            handleChange: handleInputChange,
+            label: 'Password'
+          }
+        ]
+      }}
+    />
+  )
+}
+
+export default SignIn
---
+++ apps/industry_4.0/pages/checkoutPage.tsx
@@ -0,0 +1,230 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Box, Flex, Text, Stack, Checkbox } from '@chakra-ui/react'
+
+import { useLanguage } from '../hooks/useLanguage'
+
+import { CartItemForRequest, DataPerBpp, ICartRootState, TransactionIdRootState } from '../lib/types/cart'
+import { getCartItemsPerBpp } from '../utilities/cart-utils'
+import useRequest from '../hooks/useRequest'
+import { responseDataActions } from '../store/responseData-slice'
+import {
+  areShippingAndBillingDetailsSame,
+  getPayloadForInitRequest,
+  getSubTotalAndDeliveryCharges
+} from '../utilities/checkout-utils'
+import { Checkout } from '@beckn-ui/becknified-components'
+
+import { Router, useRouter } from 'next/router'
+import { ShippingFormInitialValuesType } from '@beckn-ui/becknified-components'
+
+export type ShippingFormData = {
+  name: string
+  mobileNumber: string
+  email: string
+  address: string
+  zipCode: string
+}
+
+const CheckoutPage = () => {
+  const [formData, setFormData] = useState<ShippingFormInitialValuesType>({
+    name: 'Antoine Dubois',
+    mobileNumber: '0612345678',
+    email: 'antoine.dubois@gmail.com',
+    address: '15 Rue du Soleil, Paris, France',
+    pinCode: '75001'
+  })
+
+  const [submittedDetails, setSubmittedDetails] = useState<ShippingFormInitialValuesType>({
+    name: 'Antoine Dubois',
+    mobileNumber: '0612345678',
+    email: 'antoine.dubois@gmail.com',
+    address: '15 Rue du Soleil, Paris, France',
+    pinCode: '75001'
+  })
+
+  const [isBillingAddressSameAsShippingAddress, setIsBillingAddressSameAsShippingAddress] = useState(true)
+
+  const [billingFormData, setBillingFormData] = useState<ShippingFormInitialValuesType>({
+    name: 'Antoine Dubois',
+    mobileNumber: '0612345678',
+    email: 'antoine.dubois@gmail.com',
+    address: '15 Rue du Soleil, Paris, France',
+    pinCode: '75001'
+  })
+
+  const router = useRouter()
+  const initRequest = useRequest()
+  const dispatch = useDispatch()
+  const { t, locale } = useLanguage()
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL
+  const cartItems = useSelector((state: ICartRootState) => state.cart.items)
+  const transactionId = useSelector((state: { transactionId: TransactionIdRootState }) => state.transactionId)
+
+  useEffect(() => {
+    if (localStorage) {
+      if (localStorage.getItem('userPhone')) {
+        const copiedFormData = structuredClone(formData)
+        const copiedBillingFormData = structuredClone(billingFormData)
+
+        copiedFormData.mobileNumber = localStorage.getItem('userPhone') as string
+        copiedBillingFormData.mobileNumber = localStorage.getItem('userPhone') as string
+
+        setFormData(copiedFormData)
+        setBillingFormData(copiedBillingFormData)
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('shippingAdress')) {
+        setFormData(JSON.parse(localStorage.getItem('shippingAdress') as string))
+      }
+      if (localStorage.getItem('billingAddress')) {
+        setBillingFormData(JSON.parse(localStorage.getItem('billingAddress') as string))
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (initRequest.data) {
+      if (typeof window !== 'undefined') {
+        localStorage.setItem('initResult', JSON.stringify(initRequest.data))
+      }
+
+      dispatch(responseDataActions.addInitResponse(initRequest.data))
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [initRequest.data])
+
+  useEffect(() => {
+    const shippingAddressComplete = Object.values(formData).every(value => value.length > 0)
+    if (shippingAddressComplete && typeof window !== 'undefined') {
+      localStorage.setItem('shippingAdress', JSON.stringify(formData))
+    }
+  }, [formData])
+
+  useEffect(() => {
+    const isBillingAddressComplete = Object.values(billingFormData).every(value => value.length > 0)
+
+    if (isBillingAddressComplete && typeof window !== 'undefined') {
+      localStorage.setItem('billingAddress', JSON.stringify(billingFormData))
+    }
+    setIsBillingAddressSameAsShippingAddress(
+      areShippingAndBillingDetailsSame(isBillingAddressComplete, formData, billingFormData)
+    )
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [billingFormData])
+
+  const formSubmitHandler = data => {
+    if (data) {
+      // TODO :_ To check this again
+
+      // if (isBillingAddressSameAsShippingAddress) {
+      //   const copiedFormData = structuredClone(data);
+      //   setBillingFormData(copiedFormData);
+      // }
+
+      const cartItemsPerBppPerProvider: DataPerBpp = getCartItemsPerBpp(cartItems as CartItemForRequest[])
+
+      const payLoadForInitRequest = getPayloadForInitRequest(
+        cartItemsPerBppPerProvider,
+        transactionId,
+        data,
+        billingFormData
+      )
+      initRequest.fetchData(`${apiUrl}/client/v2/initialize_order`, 'POST', payLoadForInitRequest)
+    }
+  }
+
+  const isInitResultPresent = () => {
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('initResult')) {
+        return true
+      }
+    }
+
+    return !!initRequest.data
+  }
+
+  return (
+    <>
+      {/* start Item Details */}
+      <Checkout
+        schema={{
+          items: {
+            title: 'Items',
+            data: cartItems.map(singleItem => ({
+              title: singleItem.descriptor.name,
+              description: singleItem.descriptor.short_desc,
+              quantity: singleItem.quantity,
+              priceWithSymbol: `${t.currencySymbol}${singleItem.totalPrice}`
+            }))
+          },
+          shipping: {
+            showDetails: isInitResultPresent(),
+            shippingDetails: {
+              name: submittedDetails.name,
+              location: submittedDetails.address,
+              number: submittedDetails.mobileNumber,
+              title: 'Shipping'
+            },
+            shippingForm: {
+              onSubmit: formSubmitHandler,
+              submitButton: { text: 'Save Shipping Details' },
+              values: formData,
+              onChange: data => setSubmittedDetails(data)
+            }
+          },
+          billing: {
+            sectionSubtitle: 'Add Billing Details',
+            sectionTitle: 'Billing',
+            formTitle: 'Add Billing Details',
+            isBilling: true,
+            showDetails: isInitResultPresent(),
+            shippingDetails: {
+              name: submittedDetails.name,
+              location: submittedDetails.address,
+              number: submittedDetails.mobileNumber,
+              title: 'Billing'
+            },
+            shippingForm: {
+              onSubmit: formSubmitHandler,
+              submitButton: { text: 'Save Shipping Details' },
+              values: formData,
+              onChange: data => setSubmittedDetails(data)
+            }
+          },
+          payment: {
+            title: 'Payment',
+            paymentDetails: {
+              subtotalText: 'Subtotal',
+              deliveryChargesText: 'Delivery Charges',
+              deliveryChargesValue: `${t.currencySymbol} ${
+                getSubTotalAndDeliveryCharges(initRequest.data).totalDeliveryCharge
+              }`,
+              subtotalValue: `${t.currencySymbol} ${getSubTotalAndDeliveryCharges(initRequest.data).subTotal}`,
+              totalText: 'Total',
+              totalValueWithSymbol: `${t.currencySymbol}${
+                getSubTotalAndDeliveryCharges(initRequest.data).subTotal +
+                getSubTotalAndDeliveryCharges(initRequest.data).totalDeliveryCharge
+              }`
+            }
+          },
+          loader: {
+            text: 'Initializing your request'
+          },
+          pageCTA: {
+            text: 'Proceed to Checkout',
+            handleClick: () => router.push('/paymentMode')
+          }
+        }}
+        isLoading={initRequest.loading}
+        hasInitResult={isInitResultPresent()}
+      />
+    </>
+  )
+}
+export default CheckoutPage
