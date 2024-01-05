ce32b61ba35df588e4f32e0510ff6681eec95b67
ujjwalEminds <ujjwal.tiwari@eminds.ai>
2024-01-05 15:54:41 +0530

    feat: implememnted feedback page

--- apps/industry_4.0/locales/en.ts
+++ apps/industry_4.0/locales/en.ts
@@ -78,6 +78,14 @@ const en: { [key: string]: string } = {
   confirmLoaderSubtext: 'while we confirm your order',
   statusLoaderSubText: 'while we update the current status of your order.',
   emptyOrderHistoryText: 'This space appears quite empty!',
-  noExistingWorkflowText: 'No existing workflows found; create a new workflow to proceed.'
+  noExistingWorkflowText: 'No existing workflows found; create a new workflow to proceed.',
+  allRequestFullfilled: 'All orders delivered!',
+  howTodo: 'How did we do?',
+  rateUs: 'Rate Us',
+  orderDeliveredOnTime: 'Order Delivered on Time!',
+  pleaseShareYourFeedback: 'Please share your feedback with us',
+  rateDeliveryExperience: 'Rate the overall experience',
+  addCommentsHere: 'Add your comments here',
+  writeExperience: 'Write about your experience..'
 }
 export default en
