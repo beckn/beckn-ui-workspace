8a0eede04d5e067ffd182394bc34976c27b5243d
Ankit Brahmbhatt <ankitbrahm.bhatt@eminds.ai>
2024-01-12 17:33:46 +0530

    feat: new ec url added

--- apps/dsnp/utilities/polka.ts
+++ apps/dsnp/utilities/polka.ts
@@ -2,5 +2,6 @@ export const parentURLs = [
   'https://experience-guide-dev.becknprotocol.io',
   'https://experience-guide-staging.becknprotocol.io',
   'https://experience-guide.becknprotocol.io',
+  'https://experience.becknprotocol.io',
   'http://localhost:3001'
 ]
