6860a7b586dbfc6b98a32a8cc7838b8d20c90112
Ujjwal Tiwari <ujjwaltiwari@Ujjwals-MacBook-Pro.local>
2024-04-12 15:04:46 +0530

    feat: added missing click event on the job card in the history page

--- apps/dsep/components/orderHistory/MyJob.tsx
+++ apps/dsep/components/orderHistory/MyJob.tsx
@@ -1,6 +1,6 @@
 import { DetailCard } from '@beckn-ui/becknified-components'
 import { Typography } from '@beckn-ui/molecules'
-import { Flex, Image } from '@chakra-ui/react'
+import { Box, Flex, Image } from '@chakra-ui/react'
 import React from 'react'
 import { useLanguage } from '../../hooks/useLanguage'
 
@@ -14,43 +14,45 @@ interface MyJobsProps {
 const MyJob: React.FC<MyJobsProps> = props => {
   const { t } = useLanguage()
   return (
-    <DetailCard>
-      <Typography
-        text={props.heading}
-        fontWeight={'600'}
-        style={{
-          paddingBottom: '5px'
-        }}
-      />
-      <Flex
-        alignItems={'center'}
-        justifyContent="space-between"
-        pt={'5px'}
-      >
+    <Box onClick={props.handleJobsStatus}>
+      <DetailCard>
         <Typography
+          text={props.heading}
+          fontWeight={'600'}
           style={{
             paddingBottom: '5px'
           }}
-          text={`${t.appliedOn} ${props.time}`}
         />
-        <Flex alignItems={'center'}>
-          {props.myJobsStatus === 'In Review' ? (
-            <Image
-              src="/images/inProgress.svg"
-              alt="in progress icon"
-              pr="10px"
-            />
-          ) : (
-            <Image
-              src="/images/approvedIcon.svg"
-              alt="approve icon"
-              pr="10px"
-            />
-          )}
-          <Typography text={props.myJobsStatus} />
+        <Flex
+          alignItems={'center'}
+          justifyContent="space-between"
+          pt={'5px'}
+        >
+          <Typography
+            style={{
+              paddingBottom: '5px'
+            }}
+            text={`${t.appliedOn} ${props.time}`}
+          />
+          <Flex alignItems={'center'}>
+            {props.myJobsStatus === 'In Review' ? (
+              <Image
+                src="/images/inProgress.svg"
+                alt="in progress icon"
+                pr="10px"
+              />
+            ) : (
+              <Image
+                src="/images/approvedIcon.svg"
+                alt="approve icon"
+                pr="10px"
+              />
+            )}
+            <Typography text={props.myJobsStatus} />
+          </Flex>
         </Flex>
-      </Flex>
-    </DetailCard>
+      </DetailCard>
+    </Box>
   )
 }
 
