4fb3a98ae81fc2a8564637995625b1d072dc269a
Ankit Brahmbhatt <ankitbrahm.bhatt@eminds.ai>
2024-05-06 19:56:43 +0530

    fix: dsnp-bugs-fix

--- apps/dsnp-v2/components/header/index.tsx
+++ apps/dsnp-v2/components/header/index.tsx
@@ -94,7 +94,7 @@ const bottomHeaderBlackList = ['/orderConfirmation', '/']
 const menuIconWhiteList = ['/', '/search', '/profile']
 const orderIconList = ['/orderDetails']
 const editIcon = ['/profile']
-const invoiceDownloadIcon = ['/invoiceDetails']
+const invoiceDownloadIcon = ['']
 const currentLocation = ['/']
 
 const getHeaderTitleForPage = (name: string, logo: string, pathName: string, locale: string | undefined) => {
--- apps/dsnp-v2/pages/profile.tsx
+++ apps/dsnp-v2/pages/profile.tsx
@@ -1,12 +1,16 @@
 import { BecknAuth } from '@beckn-ui/becknified-components'
+import Cookies from 'js-cookie'
 import { Box } from '@chakra-ui/react'
 import { profilePageProp } from '@components/signIn/SignIn.types'
 import { useLanguage } from '@hooks/useLanguage'
 import { FormErrors, profileValidateForm } from '@utils/form-utils'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ProfilePage = () => {
   const { t } = useLanguage()
+  const bearerToken = Cookies.get('authToken')
+  const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_URL
+  const [isLoading,setIsLoading] = useState(false);
   const [formData, setFormData] = useState<profilePageProp>({
     name: '',
     mobileNumber: '',
@@ -24,6 +28,76 @@ const ProfilePage = () => {
     email: '',
     zipCode: ''
   })
+
+  useEffect(()=>{
+    const myHeaders = new Headers()
+    myHeaders.append('Authorization', `Bearer ${bearerToken}`)
+
+    const requestOptions: RequestInit = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    }
+    setIsLoading(true)
+
+    fetch(`${strapiUrl}/profiles?populate[0]=documents.attachment&populate[1]=skills`, requestOptions)
+    .then(response => response.json())
+    .then((result)=>{
+      const {name,phone} = result.data.attributes
+      setFormData({
+        ...formData,
+        name,
+        mobileNumber:phone
+      })
+    }).finally(()=>{
+
+    setIsLoading(false)
+    })
+
+  },[])
+
+
+  const updateProfile = ()=>{
+    const myHeaders = new Headers()
+    myHeaders.append('Authorization', `Bearer ${bearerToken}`)
+    
+    setIsLoading(true)
+
+    const currentFormData = new FormData();
+const data = {
+  name:formData.name,
+  phone:formData.mobileNumber,
+  address: "TX, DALLAS",
+  documents: {
+    set: [6]
+  },
+  skills: {
+    set: [1]
+  }
+};
+
+currentFormData.append('data', JSON.stringify(data));
+
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      redirect: 'follow',
+      body:currentFormData
+     }
+
+    fetch(`${strapiUrl}/profiles`, requestOptions)
+    .then(response => response.json())
+    .then((result)=>{
+      // console.log('Dank 2',result)
+      // const {name,phone} = result.data.attributes
+      
+
+    }).finally(()=>{
+      setIsLoading(false)
+
+
+    })
+  }
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
@@ -55,10 +129,15 @@ const ProfilePage = () => {
     >
       <BecknAuth
         schema={{
+          loader: { text: "Updating profile" },
           buttons: [
             {
               text: t.saveContinue,
-              handleClick: () => {},
+              handleClick: () => {
+                console.log("Dank",formData)
+                updateProfile()
+
+              },
               disabled: false,
               variant: 'solid',
               colorScheme: 'primary'
@@ -134,6 +213,7 @@ const ProfilePage = () => {
             }
           ]
         }}
+        isLoading={isLoading}
       />
     </Box>
   )
