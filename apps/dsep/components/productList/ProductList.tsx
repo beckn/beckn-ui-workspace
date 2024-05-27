854d26ac2c45b6b17e87ae1d27473f3a55ce6467
skrushna1704 <skrushna1704@gmail.com>
2024-05-27 17:04:12 +0530

    fix: dsep payment breakdown issue and added empty catalouge text

--- apps/dsep/components/productList/ProductList.tsx
+++ apps/dsep/components/productList/ProductList.tsx
@@ -5,6 +5,7 @@ import { Box } from '@chakra-ui/react'
 import { ParsedItemModel } from '../../types/search.types'
 import { ProductCard } from '@beckn-ui/becknified-components'
 import ProductCardRenderer from '../productCard/product-card-renderer'
+import { Typography } from '@beckn-ui/molecules'
 
 interface ProductListPropsModel {
   productList: ParsedItemModel[]
@@ -59,13 +60,20 @@ const ProductList: React.FC<ProductListPropsModel> = ({ productList }) => {
           selectedBtn={selectedRadioBtn}
         />
         <Box marginTop={'107px'}>
-          {sortedProductList.map((item, idx) => (
-            <ProductCard
-              key={idx}
-              ComponentRenderer={ProductCardRenderer}
-              dataSource={item}
+          {sortedProductList.length ? (
+            sortedProductList.map((item, idx) => (
+              <ProductCard
+                key={idx}
+                ComponentRenderer={ProductCardRenderer}
+                dataSource={item}
+              />
+            ))
+          ) : (
+            <Typography
+              text={t.noProduct}
+              variant="subTitleRegular"
             />
-          ))}
+          )}
         </Box>
       </Box>
     </>
