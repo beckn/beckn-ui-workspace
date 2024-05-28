4c7d93b71e8a4d6c689abe4ca2911e5a0fe6a462
Ankit Brahmbhatt <ankitbrahm.bhatt@eminds.ai>
2024-05-28 12:27:35 +0530

    fix: dsnp bugs fixes

--- apps/dsnp-v2/pages/product.tsx
+++ apps/dsnp-v2/pages/product.tsx
@@ -13,6 +13,7 @@ import { fromBinary, isEmpty } from '@utils/common-utils'
 import { useRouter } from 'next/router'
 import { useSearchParams } from 'next/navigation'
 import { DiscoveryRootState } from '@store/discovery-slice'
+import CustomerReviews from '@components/UI/customerReviews/CustomerReviews'
 import { Box, Flex, useTheme } from '@chakra-ui/react'
 import { Button, CustomThemeType, Input, Typography } from '@beckn-ui/molecules'
 import axios from 'axios'
@@ -24,10 +25,12 @@ const Product = () => {
   const { isMobile, isTablet, isDesktop } = useResponsive()
   const dispatch = useDispatch()
   const [counter, setCounter] = useState(1)
+  const [dsnpUserMap, setDsnpUserMap] = useState(new Map())
   const [totalPrice, setTotalPrice] = useState('0')
   const router = useRouter()
   const url = new URL(window.location.href)
   const reviewSubmitted = url.searchParams.get('reviewSubmitted')
+  const [feed, setFeed] = useState([])
   const productName = url.searchParams.get('productName')
   const productImage = url.searchParams.get('productImage')
 
@@ -53,6 +56,63 @@ const Product = () => {
       }
     }
   }
+  useEffect(() => {
+    getReviews().then(res => {
+      setFeed(
+        res.filter(singleFeed => {
+          const content = JSON.parse(singleFeed.content)
+          if (content.tag && content.tag.length > 0) {
+            const url = new URL(content.tag[0].href)
+            const productId = url.searchParams.get('productId')
+            const productName = url.searchParams.get('productName')
+            const isProductReview = selectedProduct.item.name === productName
+            return isProductReview
+          }
+        })
+      )
+    })
+  }, [])
+
+  useEffect(() => {
+    if (Object.keys(dsnpUserMap).length === 0 && !isEmpty(feed)) {
+      const ids = Array.from(
+        new Set(
+          feed.map(singleFeed => {
+            return singleFeed.fromId
+          })
+        )
+      )
+      getDsnpProfiles(ids).then(profiles => setDsnpUserMap(profiles))
+    }
+  }, [feed])
+
+  const getDsnpProfiles = async ids => {
+    const { accessToken, dsnpId } = getLocalStorage('dsnpAuth')
+    const idMap = new Map(ids.map(id => [id, '']))
+
+    if (accessToken) {
+      try {
+        const responseList = await Promise.all(
+          ids.map(singleId => {
+            return axios.request({
+              url: `https://api.dsnp-social-web.becknprotocol.io/v1/profiles/${singleId}`,
+              method: 'GET',
+              headers: {
+                Authorization: `Bearer ${accessToken}`
+              }
+            })
+          })
+        )
+        responseList.forEach(singleProfile => {
+          idMap.set(singleProfile.data.fromId, singleProfile.data.displayHandle)
+        })
+        return idMap
+      } catch (err) {
+        console.log('Error', err)
+        throw Error(err)
+      }
+    }
+  }
 
   const increment = () => {
     const newCounter = counter + 1
@@ -70,6 +130,21 @@ const Product = () => {
     }
   }
 
+  const renderedReviews = feed.map((singleFeed, index) => {
+    const content = JSON.parse(singleFeed.content)
+    const name = dsnpUserMap.get(singleFeed.fromId)
+    return (
+      <CustomerReviews
+        key={index}
+        name={name}
+        // rating={review.rating}
+        // reviewHeading={review.reviewHeading}
+        review={content.content}
+        // foundHelpful={review.helpFul}
+      />
+    )
+  })
+
   if (!selectedProduct) {
     return <></>
   }
@@ -86,41 +161,44 @@ const Product = () => {
           productName={productName as string}
         />
       ) : (
-        <ProductDetailPage
-          schema={{
-            productSummary: {
-              imageSrc: selectedProduct.item.images[0].url,
-              name: selectedProduct.item.name,
-              secondaryDescription: selectedProduct.item.long_desc,
-              starRating: {
-                rating: selectedProduct.item.rating,
-                size: 20,
-                setRating: () => {},
-                starCount: 5
-              },
-              productCta: {
-                currency: selectedProduct.item.price.currency,
-                totalPrice: selectedProduct.item.price.value,
-                handleIncrement: increment,
-                handleDecrement: decrement,
-                counter: counter,
-                cta: {
-                  text: 'Add To Cart',
-                  color: 'black',
-                  handleClick: () => {
-                    dispatch(
-                      cartActions.addItemToCart({
-                        product: selectedProduct,
-                        quantity: counter
-                      })
-                    )
-                    toast.success('Product added to cart')
+        <>
+          <ProductDetailPage
+            schema={{
+              productSummary: {
+                imageSrc: selectedProduct.item.images[0].url,
+                name: selectedProduct.item.name,
+                secondaryDescription: selectedProduct.item.long_desc,
+                starRating: {
+                  rating: selectedProduct.item.rating,
+                  size: 20,
+                  setRating: () => {},
+                  starCount: 5
+                },
+                productCta: {
+                  currency: selectedProduct.item.price.currency,
+                  totalPrice: selectedProduct.item.price.value,
+                  handleIncrement: increment,
+                  handleDecrement: decrement,
+                  counter: counter,
+                  cta: {
+                    text: 'Add To Cart',
+                    color: 'black',
+                    handleClick: () => {
+                      dispatch(
+                        cartActions.addItemToCart({
+                          product: selectedProduct,
+                          quantity: counter
+                        })
+                      )
+                      toast.success('Product added to cart')
+                    }
                   }
                 }
               }
-            }
-          }}
-        />
+            }}
+          />
+          <Box>{renderedReviews}</Box>
+        </>
       )}
     </Box>
   )
