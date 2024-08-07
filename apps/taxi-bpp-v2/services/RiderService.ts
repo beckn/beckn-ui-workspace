37fa99b266d911c1ac130bc582bcf6789e8569d3
aniketceminds <aniket.chahare@eminds.ai>
2024-08-07 19:22:15 +0530

    feat(taxi-bpp): added api in redux for update location

--- apps/taxi-bpp-v2/services/RiderService.ts
+++ apps/taxi-bpp-v2/services/RiderService.ts
@@ -2,15 +2,15 @@ import Api from './api'
 import { SerializedError } from '@reduxjs/toolkit'
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query'
 
-export interface ToggleAvailabilityRequest {
-  available: boolean
+export interface RiderRequest {
+  available?: boolean
   location: { lat: string; long: string }
 }
 
-export interface ToggleAvailabilityResponse {
+export interface RiderResponse {
   data:
     | {
-        toggleAvailabiltiyResponse: Record<string, any>
+        toggleAvailabiltiyResponse?: Record<string, any>
         updateLocationResponse: Record<string, any>
       }
     | {
@@ -20,9 +20,16 @@ export interface ToggleAvailabilityResponse {
 
 const riderApi = Api.injectEndpoints({
   endpoints: build => ({
-    toggleAvailability: build.mutation<ToggleAvailabilityResponse, ToggleAvailabilityRequest>({
+    toggleAvailability: build.mutation<RiderResponse, RiderRequest>({
       query: payload => ({
         url: '/driver-app/toggle-availability',
+        method: 'POST',
+        body: payload
+      })
+    }),
+    updateLocation: build.mutation<RiderResponse, RiderRequest>({
+      query: payload => ({
+        url: '/driver-app/update-location',
         method: 'POST',
         body: payload
       })
