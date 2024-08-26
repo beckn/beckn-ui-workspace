8167d4e5b4d6504c7d99892f8da3c757c0fd400f
kundan <kdn.rthr@gmail.com>
2024-08-26 18:02:13 +0530

    feat(TOURISM): integrated header component and type models from common packages into tourism app

---
+++ apps/tourismV1.1/components/header/constants/index.ts
@@ -0,0 +1,109 @@
+type PathnameObjectType = Record<string, string>
+
+// -------------------------------- Top Header constants --------------------------------
+const appLogoBlackList = ['/signin', '/signUp']
+const homeIconBlackList = ['/', '/signin', '/mobileOtp', '/paymentMode', '/signUp']
+const menuIconWhiteList = ['/', '/search', '/profile']
+const topHeaderBlackList: string[] = []
+const languageIconWhiteList = ['/', '/createProfile', '/signin', '/signUp']
+
+// -------------------------------- Sub Header constants --------------------------------
+const backIconList = ['/', '/signin']
+
+const headerNames: PathnameObjectType = {
+  '/checkoutPage': 'Review Purchase Order',
+  '/orderHistory': 'My Orders',
+  '/orderDetails': 'Order Details',
+  '/invoiceDetails': 'Invoice Details',
+  '/signin': 'Sign In',
+  '/signUp': 'Sign Up',
+  '/cart': 'Cart',
+  '/paymentMode': 'Select Payment Method',
+  '/assemblyDetails': 'Add Assembly Details',
+  '/updateShippingDetails': 'Shipping Details',
+  '/orderCancellation': 'Order Cancel',
+  '/feedback': '',
+  '/profile': 'My Profile',
+  '/search': 'Search results',
+  '/checkout': 'Billing & Shipping'
+}
+
+const headerFrenchNames: PathnameObjectType = {
+  '/checkoutPage': 'Facturation et Livraison',
+  '/orderHistory': 'Historique des Commandes',
+  '/orderDetails': 'Détails de la Commande',
+  '/': 'Se Connecter',
+  '/mobileOtp': 'Se Connecter',
+  '/cart': 'Panier',
+  '/paymentMode': 'Sélectionner la Méthode de Paiement',
+  '/feedback': ''
+}
+
+const headerBlackList = [
+  '/checkoutPage',
+  '/orderHistory',
+  '/orderDetails',
+  '/cart',
+  '/',
+  '/orderConfirmation',
+  'feedback',
+  '/',
+  '/signUp',
+  '/mobileOtp',
+  '/paymentMode',
+  '/invoiceDetails',
+  '/assemblyDetails',
+  '/updateShippingDetails',
+  '/orderCancellation',
+  '/profile',
+  '/search',
+  '/checkout',
+  '/signin'
+]
+
+const cartIconBlackList: string[] = [
+  '/orderConfirmation',
+  '/orderDetails',
+  '/trackOrder',
+  '/feedback',
+  '/orderHistory',
+  '/signin',
+  '/mobileOtp',
+  '/checkout',
+  '/paymentMode',
+  '/signUp',
+  '/invoiceDetails',
+  '/',
+  '/cart',
+  '/profile',
+  '/orderCancellation',
+  '/updateShippingDetails',
+  '/search',
+  '/product'
+]
+
+const bottomHeaderBlackList = ['/orderConfirmation', '/', '/feedback']
+const orderIconList = ['']
+const invoiceDownloadIcon = ['/']
+const qrCodeScanerIcon = ['/orderDetails']
+
+export default {
+  TopHeader: {
+    appLogoBlackList,
+    homeIconBlackList,
+    menuIconWhiteList,
+    topHeaderBlackList,
+    languageIconWhiteList
+  },
+  SubHeader: {
+    backIconList,
+    headerNames,
+    headerFrenchNames,
+    headerBlackList,
+    cartIconBlackList,
+    bottomHeaderBlackList,
+    orderIconList,
+    invoiceDownloadIcon,
+    qrCodeScanerIcon
+  }
+}
