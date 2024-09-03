f2632590117c46341062e8c5296eb1c75d2d0f3a
kundan <kdn.rthr@gmail.com>
2024-09-03 12:44:28 +0530

    feat(TOURISM): integrated product component and type models from common packages into tourism

--- apps/tourismV1.1/pages/product.tsx
+++ apps/tourismV1.1/pages/product.tsx
@@ -1,23 +1,23 @@
 import React, { useState } from 'react'
 import { ProductDetailPage } from '@beckn-ui/becknified-components'
-
 import { useDispatch, useSelector } from 'react-redux'
-
-import { useRouter } from 'next/router'
 import { Box } from '@chakra-ui/react'
 import { useLanguage } from '@hooks/useLanguage'
-import { cartActions, DiscoveryRootState } from '@beckn-ui/common'
+import { DiscoveryRootState, ParsedItemModel } from '@beckn-ui/common/lib/types'
+import { cartActions } from '@beckn-ui/common/src/store/cart-slice'
+import { useRouter } from 'next/router'
 
 const Product = () => {
-  const selectedProduct = useSelector((state: DiscoveryRootState) => state.discovery.selectedProduct)
+  const router = useRouter()
+  const { t } = useLanguage()
+  const selectedProduct: ParsedItemModel = useSelector((state: DiscoveryRootState) => state.discovery.selectedProduct)
   const dispatch = useDispatch()
   const [counter, setCounter] = useState(1)
-  const [totalPrice, setTotalPrice] = useState(selectedProduct.item.price.value)
-  const { t } = useLanguage()
+  const [totalPrice, setTotalPrice] = useState<number>(Number(selectedProduct.item.price.value))
 
   const increment = () => {
     const newCounter = counter + 1
-    const newTotalPrice = newCounter * selectedProduct.item.price.value
+    const newTotalPrice = newCounter * Number(selectedProduct.item.price.value)
     setCounter(newCounter)
     setTotalPrice(newTotalPrice)
   }
@@ -25,12 +25,11 @@ const Product = () => {
   const decrement = () => {
     if (counter > 1) {
       const newCounter = counter - 1
-      const newTotalPrice = newCounter * selectedProduct.item.price.value
+      const newTotalPrice = newCounter * Number(selectedProduct.item.price.value)
       setCounter(newCounter)
       setTotalPrice(newTotalPrice)
     }
   }
-  const router = useRouter()
 
   if (!selectedProduct) {
     return <></>
@@ -44,11 +43,11 @@ const Product = () => {
       <ProductDetailPage
         schema={{
           productSummary: {
-            imageSrc: selectedProduct.item.images[0].url,
+            imageSrc: selectedProduct.item.images?.[0].url!,
             name: selectedProduct.item.name,
             secondaryDescription: selectedProduct.item.long_desc,
             starRating: {
-              rating: selectedProduct.item.rating,
+              rating: selectedProduct.item.rating!,
               size: 20,
               setRating: () => {},
               starCount: 5
