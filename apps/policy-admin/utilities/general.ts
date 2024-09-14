43648975e817b0f897ca0a79ccf5522c57286b31
aniketceminds <aniket.chahare@eminds.ai>
2024-09-15 00:23:53 +0530

    fix(policy-admin): token expiry handling & nav to signIn

--- apps/policy-admin/utilities/general.ts
+++ apps/policy-admin/utilities/general.ts
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken'
+
 export function formatDate(dateTime: string): string {
   const date = new Date(dateTime)
   //   const dayName = date.toLocaleDateString('en-US', { weekday: 'long' })
@@ -6,4 +8,17 @@ export function formatDate(dateTime: string): string {
   const year = date.getFullYear()
 
   return `${day}/${month}/${year}`
+}
+
+export const checkTokenExpiry = (token: any) => {
+  const decoded: any = jwt.decode(token) // Just decodes, no verification
+
+  if (decoded && decoded.exp) {
+    const currentTime = Math.floor(Date.now() / 1000)
+    if (currentTime > decoded.exp) {
+      console.log('Token has expired')
+      return true
+    }
+  }
+  return false
 }
