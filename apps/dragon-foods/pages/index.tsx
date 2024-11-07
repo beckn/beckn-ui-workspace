158bc83a6a89cace04cb935b8671150dbcf662eb
skrushna1506 <krushna.gaikwad@eminds.ai>
2024-11-07 11:44:45 +0530

    feat(climate_data): added logic for frequently access card

--- apps/dragon-foods/pages/index.tsx
+++ apps/dragon-foods/pages/index.tsx
@@ -2,57 +2,15 @@ import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { useLanguage } from '@hooks/useLanguage'
 import beckenFooter from '../public/images/footer.svg'
-import { HomePageContent, TopSheet, useGeolocation } from '@beckn-ui/common'
+import { discoveryActions, HomePageContent, ParsedItemModel, TopSheet, useGeolocation } from '@beckn-ui/common'
 import dragonMobLogo from '@public/images/dragonFoodLogo.svg'
-import skyAnalytic from '@public/images/sky1.svg'
-import flood from '@public/images/sky2.svg'
-import cinematic from '@public/images/sky3.svg'
 import FrequentlyAccessed from '../components/frequentlyAccessed/FrequentlyAccessed'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@store/index'
 
-export const frequentlyAccessedData = [
-  {
-    id: '1',
-    images: [skyAnalytic],
-    name: 'Sky Analytics',
-    shortDesc: 'High resolution probabilistic flo...',
-    price: { value: 0 },
-    rateLabel: '',
-    rating: '4.2',
-    source: 'Sold By',
-    sourceText: 'Sky Analytics',
-    productInfo: '',
-    yearsOfOperation: 7
-  },
-  {
-    id: '2',
-    images: [flood],
-    name: 'FloodCase',
-    shortDesc: 'Hydraulic model flood prediction...',
-    price: { value: 0 },
-    rateLabel: '',
-    rating: '4.2',
-    source: 'Sold By',
-    sourceText: 'FloodCase',
-    productInfo: '',
-    yearsOfOperation: 4
-  },
-  {
-    id: '3',
-    images: [cinematic],
-    name: 'Climatic',
-    shortDesc: 'Medium resolution integrated mod...',
-    price: { value: 0 },
-    rateLabel: '',
-    rating: '4.2',
-    source: 'Sold By',
-    sourceText: 'Climatic',
-    productInfo: '',
-    yearsOfOperation: 2
-  }
-]
-// ... existing code ...
 const HomePage = () => {
   const { t } = useLanguage()
+  const dispatch = useDispatch()
   const [searchTerm, setSearchTerm] = useState<string>('')
   const apiKeyForGoogle = process.env.NEXT_PUBLIC_GOOGLE_API_KEY
 
@@ -61,14 +19,42 @@ const HomePage = () => {
     error: currentLocationFetchError,
     loading: loadingForCurrentAddress
   } = useGeolocation(apiKeyForGoogle as string)
+  const userId = useSelector((state: RootState) => state.auth?.user?.id)
 
   const router = useRouter()
 
+  const [frequentlyAccessedData, setFrequentlyAccessedData] = useState<any[]>([])
+
   useEffect(() => {
-    if (localStorage) {
-      localStorage.clear()
-    }
+    const storedData = JSON.parse(localStorage.getItem('recentlyViewed') || '{}')
+    const userData = storedData[userId]?.recentlyViewed?.products || []
+    setFrequentlyAccessedData(userData)
   }, [])
+
+  const handleClickCardHandler = (selectedItem: ParsedItemModel) => {
+    const product = {
+      id: selectedItem.item.id,
+      name: selectedItem.item.name
+    }
+
+    dispatch(discoveryActions.addSingleProduct({ product: selectedItem }))
+
+    router.push({
+      pathname: '/product',
+      query: {
+        id: selectedItem.item.id,
+        search: searchTerm
+      }
+    })
+
+    localStorage.setItem('selectCardHeaderText', JSON.stringify(product.name))
+  }
+
+  // useEffect(() => {
+  //   if (localStorage) {
+  //     localStorage.clear()
+  //   }
+  // }, [])
 
   const navigateToSearchResults = () => {
     if (searchTerm) {
@@ -83,6 +69,7 @@ const HomePage = () => {
     }
     e.preventDefault()
   }
+
   return (
     <>
       <TopSheet
@@ -113,7 +100,10 @@ const HomePage = () => {
         }}
         customComponent={
           <>
-            <FrequentlyAccessed frequentlyAccessedData={frequentlyAccessedData} />
+            <FrequentlyAccessed
+              frequentlyAccessedData={frequentlyAccessedData}
+              onCardClick={handleClickCardHandler}
+            />
           </>
         }
       />
