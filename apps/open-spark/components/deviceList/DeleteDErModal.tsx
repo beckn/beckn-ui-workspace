c2015fe8af7f56527095150545932929227b0d4f
skrushna1506 <krushna.gaikwad@eminds.ai>
2024-12-06 16:39:03 +0530

    feat(open-spark): added changes as per figma for my ders page 1. added add new ders bottom modal ui 2. added delete ders bottom modal ui 3. added get post and delete api logic only

---
+++ apps/open-spark/components/deviceList/DeleteDErModal.tsx
@@ -0,0 +1,58 @@
+import BottomModalScan from '@beckn-ui/common/src/components/BottomModal/BottomModalScan'
+import { Typography } from '@beckn-ui/molecules'
+import BecknButton from '@beckn-ui/molecules/src/components/button'
+import { Box, Flex } from '@chakra-ui/react'
+import React from 'react'
+interface DeleteDErModalProps {
+  isOpen: boolean
+  onClose: () => void
+  handleConfirmDeleteDevice: () => void
+  isLoading?: boolean
+}
+
+const DeleteDErModal = (props: DeleteDErModalProps) => {
+  const { isOpen, onClose, handleConfirmDeleteDevice, isLoading } = props
+  return (
+    <Box>
+      <BottomModalScan
+        isOpen={isOpen}
+        onClose={onClose}
+        modalHeader="Attention"
+      >
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDir="column"
+          padding="0 20px 0 20px"
+          gap="10px"
+        >
+          <Typography
+            text="Are you sure you want to delete"
+            fontSize="18px"
+            fontWeight="400"
+          />
+          <Typography
+            text="this item?"
+            fontSize="18px"
+            fontWeight="400"
+            sx={{ marginBottom: '10px' }}
+          />
+          <BecknButton
+            children="Yes I’m Sure"
+            variant="solid"
+            handleClick={handleConfirmDeleteDevice}
+            isLoading={isLoading}
+          />
+          <BecknButton
+            children="Cancel"
+            variant="outline"
+            color="#E93324"
+            handleClick={onClose}
+          />
+        </Flex>
+      </BottomModalScan>
+    </Box>
+  )
+}
+
+export default DeleteDErModal
