75ed64e2518db79a8f9dc27a4f4ef0ed6b47b23c
aniketceminds <aniket.chahare@eminds.ai>
2025-01-27 19:30:06 +0530

    feat(open-spark-admin): project setup for admin portal open-spark

---
+++ apps/open-spark-admin/lib/config.ts
@@ -0,0 +1,20 @@
+export const currencyMap = {
+  EUR: '€',
+  INR: '₹',
+  USD: '$',
+  GMD: 'D'
+}
+
+export enum ROLE {
+  'ADMIN' = 'ADMIN',
+  'CONSUMER' = 'CONSUMER',
+  'PRODUCER' = 'PRODUCER'
+}
+
+export const ROUTE_TYPE = {
+  ADMIN: '/beckn-trade-bap',
+  CONSUMER: '/beckn-trade-bap',
+  PRODUCER: '/beckn-trade-bpp'
+}
+
+export const DOMAIN = 'uei:p2p_trading'
