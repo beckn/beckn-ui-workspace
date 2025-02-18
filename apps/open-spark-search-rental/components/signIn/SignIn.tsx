81ff873ce83c6e00b12ee075fa4b24e13029374c
Ankit Brahmbhatt <ankit.brahmbhatt@eminds.ai>
2025-02-18 12:12:42 +0530

    feat: open spark search rental flow

---
+++ apps/open-spark-search-rental/components/signIn/SignIn.tsx
@@ -0,0 +1,118 @@
+import React, { useState, useMemo } from 'react'
+import { BecknAuth } from '@beckn-ui/becknified-components'
+import { FormErrors, SignInFormProps } from '@beckn-ui/common/lib/types'
+import { signInValidateForm } from '@beckn-ui/common'
+import openSpark from '@public/images/spark-log.svg'
+import { useLanguage } from '@hooks/useLanguage'
+import { Box } from '@chakra-ui/react'
+import Router from 'next/router'
+import { useTradeLoginMutation } from '@services/UserService'
+import { useDispatch, useSelector } from 'react-redux'
+import { AuthRootState } from '@store/auth-slice'
+import { ROLE } from '@lib/config'
+
+const SignIn = ({ initialFormData = { email: '', password: '' } }) => {
+  const [formData, setFormData] = useState<SignInFormProps>(initialFormData)
+  const [formErrors, setFormErrors] = useState<FormErrors>({ email: '', password: '' })
+
+  const [tradeLogin, { isLoading }] = useTradeLoginMutation()
+  const { t } = useLanguage()
+  const dispatch = useDispatch()
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: value
+    }))
+
+    const updatedFormData = {
+      ...formData,
+      [name]: value
+    }
+
+    const errors = signInValidateForm(updatedFormData)
+    setFormErrors(prevErrors => ({
+      ...prevErrors,
+      [name]: t[`${errors[name as keyof FormErrors]}`] || ''
+    }))
+  }
+
+  const isFormFilled = useMemo(() => {
+    return (
+      Object.values(formData).every(value => value !== '') && Object.values(formErrors).every(value => value === '')
+    )
+  }, [formData, formErrors])
+
+  const handleSignIn = async () => {
+    const signInData = {
+      email: formData.email,
+      password: formData.password
+    }
+
+    try {
+      await tradeLogin(signInData).unwrap()
+    } catch (error) {
+      console.error('An error occurred:', error)
+    }
+  }
+
+  const handleSignUp = () => {
+    Router.push('/signUp')
+  }
+
+  return (
+    <Box>
+      <BecknAuth
+        schema={{
+          logo: {
+            src: openSpark,
+            alt: 'openSpark-logo'
+          },
+          buttons: [
+            {
+              text: t.signIn,
+              handleClick: handleSignIn,
+              disabled: !isFormFilled,
+              variant: 'solid',
+              colorScheme: 'primary',
+              isLoading: isLoading,
+              dataTest: 'login-button'
+            },
+            {
+              text: t.signUp,
+              handleClick: handleSignUp,
+              variant: 'outline',
+              colorScheme: 'primary',
+              disabled: isLoading,
+              dataTest: 'register-button'
+            }
+          ],
+          inputs: [
+            {
+              type: 'text',
+              name: 'email',
+              label: t.enterEmailID,
+              value: formData.email,
+              handleChange: handleInputChange,
+              error: formErrors.email,
+              dataTest: 'input-email'
+            },
+            {
+              type: 'password',
+              name: 'password',
+              label: t.enterPassword,
+              value: formData.password,
+              handleChange: handleInputChange,
+              error: formErrors.password,
+              dataTest: 'input-password'
+            }
+          ]
+        }}
+      />
+    </Box>
+  )
+}
+
+export default SignIn
---
+++ apps/open-spark-search-rental/pages/index.tsx
@@ -0,0 +1,181 @@
+import { Box, Flex, Image, Select } from '@chakra-ui/react'
+import Carousel from '@components/carasoul/Carousel'
+import profileIcon from '@public/images/user_profile.svg'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import { TiShoppingCart } from 'react-icons/ti'
+import { TbHexagonLetterO } from 'react-icons/tb'
+import { SlEnergy } from 'react-icons/sl'
+import { TbCertificate } from 'react-icons/tb'
+import { Typography } from '@beckn-ui/molecules'
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import ShadowCardButton from '@components/buttonCard/ShadowCardButton'
+import BecknButton from '@beckn-ui/molecules/src/components/button/Button'
+import OpenWalletBottomModal from '@components/modal/OpenWalletBottomModal'
+import { buttonStyles, images } from '@components/constant'
+import { useDispatch, useSelector } from 'react-redux'
+import { setNavigationType } from '@store/navigation-slice'
+import Cookies from 'js-cookie'
+import { AuthRootState } from '@store/auth-slice'
+import { DegWalletDetails } from '@beckn-ui/common'
+import { UserRootState } from '@store/user-slice'
+
+const HomePage = () => {
+  const bearerToken = Cookies.get('authToken')
+  const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_URL
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [walletDetails, setWalletDetails] = useState<DegWalletDetails>()
+  const [modalType, setModalType] = useState<'wallet' | 'link' | 'otp' | 'alert' | null>(null)
+
+  const router = useRouter()
+  const { user } = useSelector((state: AuthRootState) => state.auth)
+  const { shouldShowInitialAlert } = useSelector((state: UserRootState) => state.user)
+
+  const handleModalOpen = (type: 'wallet' | 'link' | 'otp' | 'alert') => setModalType(type)
+  const handleModalClose = () => setModalType(null)
+  const dispatch = useDispatch()
+
+  const handleNavigation = (type: 'MY_STORE' | 'RENT_AND_HIRE') => {
+    dispatch(setNavigationType(type))
+    if (type === 'RENT_AND_HIRE') {
+      router.push('/rentAndHire')
+    } else {
+      router.push('/myStore')
+    }
+  }
+
+  useEffect(() => {
+    if (user && user?.deg_wallet) {
+      setWalletDetails(user.deg_wallet)
+    }
+
+    if (
+      shouldShowInitialAlert &&
+      user?.deg_wallet &&
+      (!user?.deg_wallet.energy_assets_consent ||
+        !user?.deg_wallet.energy_identities_consent ||
+        !user?.deg_wallet.energy_transactions_consent)
+    ) {
+      setModalType('alert')
+    }
+  }, [user, shouldShowInitialAlert])
+
+  return (
+    <Box
+      backgroundColor="white"
+      className="hideScroll"
+      maxH={'calc(100vh - 10px)'}
+      overflowY="scroll"
+      ml={'-20px'}
+      mr={'-20px'}
+    >
+      <Flex
+        justifyContent={'space-between'}
+        alignItems={'center'}
+        mt={'20px'}
+        mb={'15px'}
+        pl={'20px'}
+        pr={'20px'}
+      >
+        <Box>
+          <Image
+            src={profileIcon}
+            alt="profileIcon"
+            onClick={() => router.push('/profile')}
+          />
+        </Box>
+        <Box>
+          {user?.deg_wallet ? (
+            <Select
+              variant="unstyled"
+              placeholder={`/subj****${user?.deg_wallet.deg_wallet_id.slice(-4)}`}
+              value=""
+              style={{
+                pointerEvents: 'none'
+              }}
+            />
+          ) : (
+            <BecknButton
+              text="Connect Wallet"
+              handleClick={() => handleModalOpen('wallet')}
+              sx={{
+                width: '93px',
+                height: '30px',
+                fontSize: '10px',
+                fontWeight: '400',
+                padding: '10px',
+                borderRadius: '6px',
+                mb: 'unset'
+              }}
+            />
+          )}
+        </Box>
+      </Flex>
+      <Box>
+        <Carousel images={images} />
+        <Box
+          padding={'10px'}
+          mr={'20px'}
+        >
+          <Typography
+            text={'Explore'}
+            fontSize="17px"
+            fontWeight="600"
+            sx={{ m: '10px' }}
+          />
+
+          <Flex
+            columnGap={'20px'}
+            flexDirection="column"
+          >
+            <ShadowCardButton
+              prefixIcon={<TiShoppingCart size={28} />}
+              text="Energy Marketplace"
+              textStyle="start"
+              postIcon={<MdOutlineKeyboardArrowRight />}
+              handleClick={() => handleNavigation('MY_STORE')}
+              dataTest="store_button"
+              sx={buttonStyles}
+            />
+
+            <ShadowCardButton
+              prefixIcon={<TbHexagonLetterO size={28} />}
+              text="Rental Services"
+              textStyle="start"
+              postIcon={<MdOutlineKeyboardArrowRight />}
+              handleClick={() => handleNavigation('RENT_AND_HIRE')}
+              dataTest="hire_button"
+              sx={buttonStyles}
+            />
+            {/* <ShadowCardButton
+              prefixIcon={<SlEnergy size={28} />}
+              text="Peer to Peer Energy Trading"
+              textStyle="start"
+              postIcon={<MdOutlineKeyboardArrowRight />}
+              handleClick={() => router.push('/peerToPeer')}
+              dataTest="store_button"
+              sx={buttonStyles}
+            /> */}
+            {/* <ShadowCardButton
+              prefixIcon={<TbCertificate size={28} />}
+              text="Loans & Subsidies"
+              textStyle="start"
+              postIcon={<MdOutlineKeyboardArrowRight />}
+              handleClick={() => router.push('/loanAndSubsidies')}
+              dataTest="store_button"
+              sx={buttonStyles}
+            /> */}
+          </Flex>
+        </Box>
+      </Box>
+      <OpenWalletBottomModal
+        modalType={modalType}
+        setModalType={setModalType}
+        onClose={handleModalClose}
+      />
+    </Box>
+  )
+}
+
+export default HomePage
