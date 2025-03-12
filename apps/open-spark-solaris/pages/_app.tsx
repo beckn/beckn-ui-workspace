a7b2b0e428b9e3f0c579c762439ca88bbaf3b7d7
aniketceminds <aniket.chahare@eminds.ai>
2025-03-12 15:49:53 +0530

    fix(open-spark): revert chnges related to routing issue in EC

--- apps/open-spark-solaris/pages/_app.tsx
+++ apps/open-spark-solaris/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import type { AppProps } from 'next/app'
 import Layout from '@components/layout/Layout'
 import { BecknProvider } from '@beckn-ui/molecules'
@@ -18,17 +18,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   const { t } = useLanguage()
   const router = useRouter()
 
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      window.parent.postMessage({ appName: 'solaris', url }, '*')
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [])
   return (
     <BecknProvider
       theme={{
