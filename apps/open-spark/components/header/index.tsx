58a7784c6b1a3473fad2155d08059e7b85d1ee18
skrushna1506 <krushna.gaikwad@eminds.ai>
2025-04-03 22:30:03 +0530

    fix(open-spark): new ui changes for open spark app 1. added new logo for open spark app 2. integrated rent catalouge api in myService page 3. remove footer image from splash 4. fixed search text append issue on searchbar for odrv2 app

--- apps/open-spark/components/header/index.tsx
+++ apps/open-spark/components/header/index.tsx
@@ -53,7 +53,7 @@ const Header = () => {
     <Box>
       {renderTopHeader && (
         <TopHeader
-          appLogo={'/images/Spark.svg'}
+          appLogo={'/images/open-spark_logo.svg'}
           t={key => t[key]}
           headerConstants={{
             blackList: {
