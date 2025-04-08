d1383bb2a16bbd08f3bec4d93fad90274598d443
aniketceminds <aniket.chahare@eminds.ai>
2025-04-08 22:52:12 +0530

    fix: fixed cypress sutomate script generate report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,25 @@
 import { defineConfig } from 'cypress'
+import cypressMochawesomeReporter from 'cypress-mochawesome-reporter/plugin'
 
 export default defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL,
     testIsolation: false,
     experimentalRunAllSpecs: true,
-    retries: {
-      runMode: 2,
-      openMode: 0
-    },
+    retries: { runMode: 2, openMode: 0 },
     setupNodeEvents(on, config) {
-      // Implement node event listeners here
+      cypressMochawesomeReporter(on) // 👈 Integrates mochawesome
+      return config
+    },
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true,
+      embeddedScreenshots: true,
+      inlineAssets: true
     }
   }
 })
