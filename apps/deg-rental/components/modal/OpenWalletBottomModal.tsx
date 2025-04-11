ea6dd7f293e984ca6f42d3fc8092d25eae2a169e
aniketceminds <aniket.chahare@eminds.ai>
2025-04-11 17:04:58 +0530

    fix(deg-retail): changed vault to wallet while connecting wallet

--- apps/deg-rental/components/modal/OpenWalletBottomModal.tsx
+++ apps/deg-rental/components/modal/OpenWalletBottomModal.tsx
@@ -145,7 +145,7 @@ const OpenWalletBottomModal: React.FC<OpenWalletBottomModalProps> = ({ modalType
   const walletOptions = [
     { text: 'MetaMask Wallet', image: metaMaskWallet, handleClick: () => console.log('meta') },
     { text: 'OpenSea Wallet', image: openSeaWallet, handleClick: () => console.log('opensea') },
-    { text: 'Vault', image: openWallet, handleClick: () => setModalType('link') }
+    { text: 'Wallet', image: openWallet, handleClick: () => setModalType('link') }
   ]
 
   return (
@@ -190,7 +190,7 @@ const OpenWalletBottomModal: React.FC<OpenWalletBottomModalProps> = ({ modalType
           case 'link':
             return (
               <BottomModal
-                title="Link to your Vault profile"
+                title="Link to your Wallet profile"
                 isOpen={true}
                 onClose={() => setModalType(null)}
               >
